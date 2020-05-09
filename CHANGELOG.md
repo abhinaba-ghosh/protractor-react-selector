580bc58fae7857d6594179690aa9de47b699be13
Abhinaba Ghosh <askabhinaba@gmail.com>
2020-05-09 21:50:43 +0530

    feat: changeLog added

---
+++ CHANGELOG.md
@@ -0,0 +1,14 @@
+### Changelog
+
+# 2.2.1
+
+**_waitForReact_** support added. This should be added at the beforeAll hook in the code with reactRootElement information. This will reduce the load in the **_By_** locator every time firing a promise to check the ReactRoot is available or not.
+
+## Breaking changes
+
+- You should be using waitForReact in the beforeEach hook or BeforeAll hook.
+- You should not be passing root Element information from element(by.react) locator
+
+# 2.2.0
+
+element(by.ReactSelector) is changed to element(by.react)
--- index.d.ts
+++ index.d.ts
@@ -1,24 +1,41 @@
-import { Locator } from 'protractor';
+import { Locator } from 'protractor'
 
 declare module 'protractor' {
-	export interface ProtractorBy {
-		/**
-		 *
-		 * @param component : required
-		 * @param props : optional
-		 * @param state : optional
-		 * @param rootElement : optional
-		 *
-		 * react helps to identify web elements by react properties such as props and states
-		 *
-		 * @example
-		 * element(by.react('MyComponent', {someBooleanProp:true}, {someBooleanState: true}, '#root'))
-		 */
-		react(
-			component: string,
-			props?: {},
-			state?: {},
-			rootElement?: string
-		): Locator;
-	}
+  export interface ProtractorBy {
+    /**
+     *
+     * @param component : required
+     * @param props : optional
+     * @param state : optional
+     *
+     * react helps to identify web elements by react properties such as props and states
+     *
+     * @example
+     * element(by.react('MyComponent', {someBooleanProp:true}, {someBooleanState: true}))
+     */
+    react(
+      component: string,
+      props?: {},
+      state?: {},
+      rootElement?: string
+    ): Locator
+  }
+
+  export interface ProtractorBrowser {
+    /**
+     *
+     *const App = () => (
+     *<div id='mount'>
+     * <MyComponent />
+     *<MyComponent someBooleanProp={true} />
+     *</div>
+     *)
+     * @example
+     * await browser.waitForReact(10000, '#mount')
+     *
+     * If the root is set to #root, then it is not necessary to pass the rootElement
+     * For more visit: https://github.com/abhinaba-ghosh/protractor-react-selector#how-to-use-react-selector
+     */
+    waitForReact(timeOut?: number, reactRoot?: string): Promise<void>
+  }
 }
